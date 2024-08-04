db4f1a795358d6d6ed1f3fed172ef60c3c95c002
Ali Quliyev <aliquliyevv2004@gmail.com>
2024-08-04 15:15:49 +0400

    added book services

---
+++ Hr_library/Hr_library.Business/Abstract/IBookService.cs
@@ -0,0 +1,9 @@
+﻿namespace Hr_library.Business.Abstract;
+
+public interface IBookService
+{
+    void AddBook(string name,DateTime publicationDate,int quantity,List<int> authorIds,List<int> genreIds);
+    void EditBook(int id,string newName,DateTime newPublicationDate,int newQuantity,List<int>newAuthorIds,List<int>newGenreIds);
+    void DeleteBook(int id);
+    void ListBooks();
+}
